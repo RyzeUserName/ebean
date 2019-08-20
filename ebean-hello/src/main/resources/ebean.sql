cc18706ce63493722db53d4de575784690a7fe31
lifutian <lifutian>
2019-08-20 20:12:49 +0800

    １１１

---
+++ ebean-hello/src/main/resources/ebean.sql
@@ -0,0 +1,45 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : root
+ Source Server Type    : MySQL
+ Source Server Version : 50717
+ Source Host           : 127.0.0.1:3306
+ Source Schema         : ebean
+
+ Target Server Type    : MySQL
+ Target Server Version : 50717
+ File Encoding         : 65001
+
+ Date: 20/08/2019 20:11:55
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for car
+-- ----------------------------
+DROP TABLE IF EXISTS `car`;
+CREATE TABLE `car`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `colour` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `engine` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `seat` int(255) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT ' 主键',
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '名字',
+  `sex` tinyint(4) NULL DEFAULT NULL COMMENT '性别',
+  `birthday` datetime(0) NULL DEFAULT NULL COMMENT '生日',
+  `age` int(11) NULL DEFAULT NULL COMMENT '年龄',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+SET FOREIGN_KEY_CHECKS = 1;
